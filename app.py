e428165437549d7f94588397e9ecddf9a1863e8d
Hellondd <84024619+Hellondd@users.noreply.github.com>
2026-06-07 13:38:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,3 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
     return redirect(url_for("client_dashboard"))
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
